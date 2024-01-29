2064be3f23b6a32434a4f1d9767aa797428ebd04
ilf68 <ilf68@mail.ru>
2024-01-29 19:32:00 +0500

    Загрузка файлов на github [#28.01. lesson 159036]

--- src/main/java/ru/job4j/MainTest.java
+++ src/main/java/ru/job4j/MainTest.java
@@ -8,6 +8,6 @@ public class MainTest {
     public void whenActionThenResult() {
         int excepted = 1;
         int result = 1;
-        assertThat(result).isEqualTo(excepted);
+        Assertions.assertThat(result).isEqualTo(excepted);
     }
 }
