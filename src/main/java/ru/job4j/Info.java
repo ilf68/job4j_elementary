8481ac467826fd31d853c0b701866f3c16c33897
ilf68 <ilf68@mail.ru>
2024-01-29 19:44:38 +0500

    Загрузка файлов на github [#28.01. lesson 159036]

--- src/main/java/ru/job4j/Info.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j;
-
-public class Info {
-    public static void main(String[] args) {
-        System.out.println("28.01.2024"); // лучше конечно сразу научится выводить дату из системы
-    }
-}
