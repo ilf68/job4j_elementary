9b5a7879094935feb39eb784862b0da56ce3d88c
ilf68 <ilf68@mail.ru>
2024-01-29 19:53:58 +0500

    Загрузка файлов на github [#28.01. lesson 159036]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i = i + 1) {
+            System.out.println("1 * " + i + " =" + i);
+        }
+    }
+}
