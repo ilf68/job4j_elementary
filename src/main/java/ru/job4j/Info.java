371ea824a87adbbe60725e89071fa9733dd08e17
ilf68 <ilf68@mail.ru>
2024-01-30 12:02:15 +0500

    исправил комментарий в файле на английский

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("28.01.2024"); // лучше конечно сразу научится выводить дату из системы
+        System.out.println("28.01.2024"); // I need to know write a real date
     }
 }
