2555d4767a58cff2bea371ea950c23e8e876346d
ilf68 <ilf68@mail.ru>
2024-02-03 20:00:32 +0500

    from SkillBox n-factorial

--- src/main/java/ru/job4j/calculator/Factorial.java
+++ src/main/java/ru/job4j/calculator/Factorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         int i, fact = 1;
-        System.out.println("Введите любое целое число от 1 до 12:");
+        System.out.println("Введите любое целое число от 1 до 12: ");
         int value = new Scanner(System.in).nextInt();
         for (i = 1; i <= value; i++) {
             fact = fact * i;
