e9888cb35edc175dd637466a1d3dbb60078a1d62
ilf68 <ilf68@mail.ru>
2024-02-03 19:58:30 +0500

    homework 4. Тесты для стороны прямоугольника. [#159460]!

---
+++ src/main/java/ru/job4j/calculator/Factorial.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int i, fact = 1;
+        System.out.println("Введите любое целое число от 1 до 12:");
+        int value = new Scanner(System.in).nextInt();
+        for (i = 1; i <= value; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial " + value + " is " + fact);
+    }
+}
