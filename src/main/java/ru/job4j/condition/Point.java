f4317c4fec538bda4b2ccff696aa0c7e57a4c1ec
ilf68 <ilf68@mail.ru>
2024-02-18 21:20:23 +0500

    Рефакторинг - Point and PointTest for OOP

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,29 @@
 package ru.job4j.condition;
 
-public class Point {public static double distance(int x1, int y1, int x2, int y2) {
-    double xDist = Math.pow((x1 - x2), 2);
-    double yDist = Math.pow((y1 - y2), 2);
-    double result = Math.sqrt(xDist + yDist);
-    return result;
-}
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
-     public static void main(String[] args) {
-        double result1 = Point.distance(25, 45, 19, -48);
-        double result2 = Point.distance(3, 4, 2, 20);
-        System.out.println("result (0, 0) to (2, 0) " + result1);
-        System.out.println("result (3, 4) to (2, 20) " + result2);
-     }
+public class Point {
+    private int x;
+    private int y;
+    private int z;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
+    }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,52 +1,36 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.withPrecision;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PointTest {
 
     @Test
-    void when34to2i20then16Dot03() {
-        double expected = 16.03;
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 2;
-        int y2 = 20;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    public void when34to2i20then16Dot03() {
+        Assert.assertEquals(16.03, new Point(3, 4).distance(new Point(2, 20)), 0.01);
     }
+
     @Test
     void when55to10i10then7Dot07() {
-        double expected = 7.07;
-        int x1 = 5;
-        int y1 = 5;
-        int x2 = 10;
-        int y2 = 10;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(7.07, new Point(5, 5).distance(new Point(10, 10)), 0.01);
     }
+
     @Test
     void when7Minus5to7i10then15Dot0() {
-        double expected = 15;
-        int x1 = 7;
-        int y1 = -5;
-        int x2 = 7;
-        int y2 = 10;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Assert.assertEquals(15, new Point(7, -5).distance(new Point(7, 10)), 0.01);
+    }
+
+    @Test
+    void when3d_1() {
+        Assert.assertEquals(4.0,
+                new Point(0, 0, 0).distance3d(new Point(0, 0, 4)), 0.01);
     }
     @Test
-
-    void when25i45to19Minus48then93Dot19() {
-        double expected = 93.19;
-        int x1 = 25;
-        int y1 = 45;
-        int x2 = 19;
-        int y2 = -48;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    void when_3d_2() {
+        Assert.assertEquals(11.49,
+                new Point(0, 0, 0).distance3d(new Point(10, 4, 4)), 0.01);
     }
 }
