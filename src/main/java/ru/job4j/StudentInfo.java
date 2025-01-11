76861c19c7524d09f290c9ba677dcad219445007
ilf68 <ilf68@mail.ru>
2025-01-11 16:04:57 +0500

    Разбор первой программы по "взрослому" [#505151 #527914] Порой переделать быстрее, час против суток

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ilfat Akhmerov");
+        System.out.println("26.12.19");
+    }
+}
