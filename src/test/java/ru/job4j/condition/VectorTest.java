68b6d3bfdc92253bd62101daf9a8bfda0214e449
ilf68 <ilf68@mail.ru>
2025-01-18 15:53:49 +0500

    Тестирование Класса Сложения Векторов [#505262]

---
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class VectorTest {@Test
+void whenVectors58And05ThenResult5dot10() {
+    int x1 = 5, y1 = 5, x2 = 0, y2 = 5;
+    String expected = "(5, 10)";
+    String output = Vector.addVectors(x1, y1, x2, y2);
+    assertThat(output).isEqualTo(expected);
+}
+
+    @Test
+    void whenVectorsMinus12And34ThenResult22() {
+        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        String expected = "(2, 2)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectorsMinus12AndMinus34ThenResultMinus46() {
+        int x1 = -1, y1 = -2, x2 = -3, y2 = -4;
+        String expected = "(-4, -6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+}
