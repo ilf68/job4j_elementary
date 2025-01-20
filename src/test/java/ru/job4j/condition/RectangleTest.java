2e56fc670ec9b8592f618d2ea14f7e3f4179511d
ilf68 <ilf68@mail.ru>
2025-01-20 21:21:33 +0500

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

---
+++ src/test/java/ru/job4j/condition/RectangleTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    void whenLength5AndWidth7ThenDiagonal8dot6() {
+        double length = 5;
+        double width = 7;
+        double expected = 8.6;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength9AndWidth12ThenDiagonal15() {
+        double length = 9;
+        double width = 12;
+        double expected = 15.0;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength7AndWidth24ThenDiagonal25() {
+        double length = 9;
+        double width = 20;
+        double expected = 21.93;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
