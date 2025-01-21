7cedc4fcf5761226e8e9c083e93f1ef3973fe785
ilf68 <ilf68@mail.ru>
2025-01-21 09:47:51 +0500

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

--- src/test/java/ru/job4j/condition/RectangleTest.java
+++ src/test/java/ru/job4j/condition/RectangleTest.java
@@ -24,7 +24,7 @@ class RectangleTest {
     }
 
     @Test
-    void whenLength9AndWidth20ThenDiagonal521dot93() {
+    void whenLength9AndWidth20ThenDiagonal21dot93() {
         double length = 9;
         double width = 20;
         double expected = 21.93;
