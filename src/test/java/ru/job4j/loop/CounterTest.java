c85afde97fef43c40503af4916d053becb4f9f8a
ilf68 <ilf68@mail.ru>
2025-01-23 19:35:05 +0500

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,12 +1,12 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CounterTest {
 
     @Test
-    void whenStart10Finish12sumThen33() {
+    void whenSumFrom10To12Then33() {
         int start = 10;
         int finish = 12;
         int expected = 33;
@@ -15,7 +15,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart3Finish21sumThen228() {
+    void whenSumFrom3To21Then228() {
         int start = 3;
         int finish = 21;
         int expected = 228;
@@ -24,11 +24,38 @@ class CounterTest {
     }
 
     @Test
-    void whenStart3Finish1sumThen0() {
+    void whenSumFrom3To1Then0() {
         int start = 3;
         int finish = 1;
         int expected = 0;
         int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom10To12Then22() {
+        int start = 10;
+        int finish = 12;
+        int expected = 22;
+        int output = Counter.sumByEven(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom3To21Then108() {
+        int start = 3;
+        int finish = 21;
+        int expected = 108;
+        int output = Counter.sumByEven(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinus10To3ThenMinus28() {
+        int start = -10;
+        int finish = 3;
+        int expected = -28;
+        int output = Counter.sumByEven(start, finish);
         assertThat(output).isEqualTo(expected);
     }
 }
