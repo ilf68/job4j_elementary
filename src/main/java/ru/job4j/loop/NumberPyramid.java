b1e7fb5f9341aa6fc39fc35a85bc1b87710d5f9e
ilf68 <ilf68@mail.ru>
2025-01-26 15:39:23 +0500

    Числовая пирамида [#505218]

--- src/main/java/ru/job4j/loop/NumberPyramid.java
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -22,7 +22,7 @@ public class NumberPyramid {
     public static void main(String[] args) {
         System.out.println("Number pyramid of height 4:");
         draw(4);
-        System.out.println("Number pyramid of height 9:");
-        draw(9);
+        System.out.println("Number pyramid of height 8:");
+        draw(8);
     }
 }
