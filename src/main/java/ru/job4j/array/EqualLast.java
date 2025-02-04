03571c63e481b39f417ed3a845f837782d98676c
ilf68 <ilf68@mail.ru>
2025-02-04 09:20:48 +0500

    Сравнить последние элементы двух массивов [#505234 #531220]

--- src/main/java/ru/job4j/array/EqualLast.java
+++ src/main/java/ru/job4j/array/EqualLast.java
@@ -2,6 +2,10 @@ package ru.job4j.array;
 
 public class EqualLast {
     public static boolean check(int[] left, int[] right) {
-        return left[left.length - 1] == right[right.length - 1];
+        boolean rez = false;
+        if (left[left.length - 1] == right[right.length - 1]) {
+            rez = true;
+        }
+        return rez;
     }
 }
