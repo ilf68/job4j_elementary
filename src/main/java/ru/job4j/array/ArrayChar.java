a6c56d9034aa1fccb9ae34c202dd13c1c549b89b
ilf68 <ilf68@mail.ru>
2025-02-04 10:04:06 +0500

    Слово начинается с ... [#505233 #531213]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,19 +1,14 @@
 package ru.job4j.array;
 
 public class ArrayChar {
-    @SuppressWarnings({"checkstyle:WhitespaceAfter", "checkstyle:ParenPad"})
-    public static boolean startsWith(char[] word, char[] prefix) {
+       public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        if (prefix.length <= word.length & prefix.length > 0) {
-            for (int i = 0; i < prefix.length; i++) {
-                if (prefix[i] != word[i]) {
-                    result = false;
-                    break;
-                }
-            }
-        } else {
-            result = false;
-        }
+           for (int i = 0; i < prefix.length; i++) {
+               if (word[i] != prefix[i] || prefix.length > word.length) {
+                   result = false;
+                   break;
+               }
+           }
         return result;
     }
 }
