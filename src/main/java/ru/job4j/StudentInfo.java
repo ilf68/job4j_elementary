4183da2bdf395418a9bf6d3ec561fecc4d16fc98
ilf68 <ilf68@mail.ru>
2025-02-16 15:47:36 +0500

    Проверка связи

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Ilfat Akhmerov");
-        System.out.println("26.12.19");
+        System.out.println("26.12.1968");
     }
 }
